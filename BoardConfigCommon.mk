c73c844d3cd4c6228108fa25d3b40782dbff619b
Ryan Andri <ryanandri@linuxmail.org>
2019-02-08 15:20:22 +0700

    msm8937-common: switch to selinux permissive for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,7 @@ VENDOR_PATH := device/xiaomi/msm8937-common
 # Kernel
 BOARD_KERNEL_BASE		:= 0x80000000
 BOARD_KERNEL_CMDLINE 		:= androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE		+= androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE 		+= firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME 	:= Image.gz-dtb
 BOARD_KERNEL_PAGESIZE 		:=  2048
@@ -145,6 +146,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 
 # FM
 BOARD_HAVE_QCOM_FM                 := true
